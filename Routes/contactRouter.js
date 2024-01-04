fe5e39f9e152db886ce5d5cfe6cebb3d2cc47f54
MeghaPrakasah <meghatp12345@gmail.com>
2024-01-04 13:05:38 +0530

    Controller added

--- Routes/contactRouter.js
+++ Routes/contactRouter.js
@@ -1,15 +1,17 @@
 
+
 const express = require('express');
+const { getContact, putContact, postContact, deleteContact } = require('../controllers/contactcontroller');
 const router = express.Router();
+
 
 // same routes varumbho chain reethiyill ezhutham 
 
-router.route('/').get((req,res)=>{res.status(200).json({"message":"get all contacts"})}).post((req,res)=>{res.status(200).json({"message":`created contact ${req.body.wow}`})});
+router.route('/').get(getContact).post(postContact);
 
-router.route('/:id').put((req,res)=>{res.status(200).json({"message":`updated for ${req.params.id}`})});
+router.route('/:id').put(putContact);
 
-router.route('/:id').delete((req,res)=>{res.status(200).json({"message":`Deleted for ${req.params.id}`})});
+router.route('/:id').delete(deleteContact);
 
 
-
-module.exports=router;
+module.exports = router;
